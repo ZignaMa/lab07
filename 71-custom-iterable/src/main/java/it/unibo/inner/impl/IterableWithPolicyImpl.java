63cf0ac813f72a7bc8b2ac4f7761489038d8ac03
marco zignani <marco.zignani@studio.unibo.it>
2023-11-06 14:11:39 +0100

    implementato costruttore dell'es 71

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,32 @@
+package it.unibo.inner.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+import it.unibo.inner.api.IterableWithPolicy;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+
+    private List<T> larray;
+
+
+    public IterableWithPolicyImpl(T[] array) {
+        this.larray = new ArrayList<>(List.copyOf(larray));
+    }
+    
+
+    @Override
+    public Iterator iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    }
+    
+}
