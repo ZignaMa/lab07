ac98412269cc09e1f09762c1ff16e84bedb1f267
marco zignani <marco.zignani@studio.unibo.it>
2023-11-06 18:45:46 +0100

    implementata parte 1 di es 71

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,8 +1,10 @@
 package it.unibo.inner.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 import it.unibo.inner.api.IterableWithPolicy;
@@ -10,19 +12,62 @@ import it.unibo.inner.api.IterableWithPolicy;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
     private List<T> larray;
+    Predicate<T> predicate;
 
     public IterableWithPolicyImpl(T[] array) {
-        this.larray = new ArrayList<>(List.copyOf(larray));
+        this.larray = new ArrayList<>(Arrays.asList(array));
+    }
+
+    public IterableWithPolicyImpl(T[] array, Predicate<T> predicate) {
+        this.larray = new ArrayList<>(Arrays.asList(array));
     }
     
     @Override
-    public Iterator iterator() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    public Iterator<T> iterator() {
+        return new FilterIterator();
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " larray='" + getLarray() + "'" +
+            "}";
+    }
+
+
+    private String getLarray() {
+        var sb = new StringBuilder();
+        sb.append("[");
+        for (var elem : this.larray) {
+            sb.append(elem + ", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+
+    private class FilterIterator implements Iterator<T>{
+
+        private int currentIndex=0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < larray.size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return larray.get(currentIndex++);
+        }
+        
     }
 
     @Override
-    public void setIterationPolicy(Predicate filter) {
+    public void setIterationPolicy(Predicate<T> filter) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
     }
